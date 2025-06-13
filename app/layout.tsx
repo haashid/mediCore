b79af025c91bffb70fb76511063edd87e89ba490
haashid <haashid.0607@gmail.com>
2025-06-13 15:45:05 +0530

    layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'clinic Booking',
+  description: 'Created by Haashid',
+  generator: 'haashid-dev',
 }
 
 export default function RootLayout({
